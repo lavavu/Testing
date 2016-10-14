781c69a52c35c55f832c3396ee6c0617b237a263
Owen Kaluza <owen.kaluza@monash.edu>
2016-10-14 15:30:57 +1100

    Simplify test scripts
    Added clearimages function to lavavu module

--- arrows/runtest.py
+++ arrows/runtest.py
@@ -1,30 +1,16 @@
 #!/usr/bin/env python
 import lavavu
-import os
 
 #This tests multiple db scan from filenames with sequential numbering
 output = ["test-00050.png", "test-00100.png", "test-00150.png", 
           "VectorArrows_0-00050.png", "VectorArrows_0-00100.png", "VectorArrows_0-00150.png"]
-def test(lv):
-    #Compare the output to expected results
-    global output
-    lv.testimages(output)
-
-def clean():
-    global output
-    try:
-        for f in output:
-            os.remove(f)
-    except:
-        pass
 
 dbfile = "dbFig_Base_50.gldb"
 
 #Using automated image output
-clean()
 lv = lavavu.Viewer(arglist=["scan"], writeimage=True, database=dbfile, quality=2, port=0)
 
-test(lv)
-
-clean()
+#Compare the output to expected results
+lv.testimages(output)
+lv.clearimages(output)
 
--- uw1-viewports/runtest.py
+++ uw1-viewports/runtest.py
@@ -1,37 +1,24 @@
 #!/usr/bin/env python
 import lavavu
-import os
 
 #This test ensures legacy viewport support in image output works correctly
 output = ["window-00300.png", "window-00400.png", "window-00500.png"]
-def test(lv):
-    #Compare the output to expected results
-    global output
-    lv.testimages(output)
-
-def clean():
-    global output
-    try:
-        for f in output:
-            os.remove(f)
-    except:
-        pass
 
 dbfile = "uw1-vp-gLucifer.gldb"
 
 #Using automated image output
-clean()
 lv = lavavu.Viewer(writeimage=True, timestep=[300, 500], database=dbfile, figure=-1, quality=3, port=0)
 
-test(lv)
+lv.testimages(output)
+lv.clearimages(output)
 
 #Again using "images" command
 lv = lavavu.Viewer(database=dbfile, quality=3, port=0)
 lv.timestep(300)
 lv.images(500)
 
-test(lv)
-clean()
+lv.testimages(output)
+lv.clearimages(output)
 
 #Using image output scripted through python 
 lv = lavavu.Viewer(quality=3, port=0)
@@ -43,5 +30,5 @@ for ts in range(300,501,100):
     lv.timestep(ts)
     lv.image("window-" + str(ts).zfill(5))
 
-test(lv)
-clean()
+lv.testimages(output)
+lv.clearimages(output)
