076a9467fa2f51050f71c86acefc517f799d8dfc
Owen Kaluza <owen.kaluza@monash.edu>
2017-06-01 13:39:34 +1000

    Adding a basic volume render test

---
+++ volume/runtest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import lavavu
+
+lv = lavavu.Viewer(resolution=[800,300])
+
+#lv["depthtest"] = False
+#lv["gpucache"] = True
+
+#Test volume render, with colour map and translation / rotation
+vols = []
+for x in range(-1,2):
+    lv.voltest()
+    cbar = lv.getobject() #Get last added = colourbar
+    lv.delete(cbar.id)
+    vol = lv.getobject() #Get last added = volume
+    vols.append(vol)
+    vol["translate"] = [x, 0, 0]
+    print vol["translate"]
+
+vols[0]["rotate"] = lavavu.rotateQ(0, 45, 0)
+vols[2]["rotate"] = lavavu.rotateQ(45, 0, 0)
+
+lv.images()
+
+#lv.interactive()
+
+#Compare the output to expected results
+lv.testimages()
