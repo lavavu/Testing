18375c7338bb31aea78b545499f258ad8a578b2a
Owen Kaluza <owen.kaluza@monash.edu>
2017-07-12 12:52:16 +1000

    Added tracers test

---
+++ tracers/runtest.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import lavavu
+
+#This tests multiple db scan from filenames with sequential numbering
+dbfile = "Tracers.gldb"
+
+#Using automated image output
+lv = lavavu.Viewer(database=dbfile, quality=1)
+
+#lv.scale('tracers', 'up')
+#lv.scale('tracers', 'up')
+
+lv.open()
+
+for ts in range(0,11,5):
+    lv.timestep(ts)
+    lv.image("window-" + str(ts).zfill(5))
+    #lv.image()
+
+#Compare the output to expected results
+lv.testimages()
+
