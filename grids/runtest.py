527477f5c08d39ef19cb11b35fd79cdb29dbad5b
Owen Kaluza <owen.kaluza@monash.edu>
2018-05-31 23:30:54 +1000

    Tests for grid surface rendering

---
+++ grids/runtest.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+#Test grid/quads plotting by creating the same image in several ways
+
+#note: quads renderer accepts vertices as actual quads or a grid (with dims provided)
+# quads are defined by four vertices traversed counter-clockwise
+# grids are defined from top left (eg: x=0, y=1.0), row by row
+
+import lavavu
+#lv = lavavu.Viewer(background="white", border=0, axis=False)
+#quads = lv.quads("mesh", cullface=True)
+
+import random
+random.seed(0) # Set the random number generator to a fixed sequence.
+
+def createGrid(verts, indices=None, **kwargs):
+    global lv, quads
+    #TODO: fix these, both seem to have bugs
+    #lv.clear(True)
+    #quads.clear()
+    #for key in kwargs:
+    #    quads[key] = kwargs[key]
+
+    #Re-create, works around issues with clear() for now
+    lv = lavavu.Viewer(background="white", border=0, axis=False, resolution=(300,300))
+    quads = lv.quads("mesh", cullface=True, **kwargs)
+
+    print(lv.objects)
+    quads.colourmap("diverge");
+    quads["colourby"] = "field"
+    quads.vertices(verts)
+    #Test colour vals
+    vals = [random.uniform(-0.5,0.5), random.uniform(-0.5,0.5), random.uniform(-0.5,0.5), random.uniform(-0.5,0.5)]
+    print(vals)
+    quads.values(vals, "field")
+    if indices is not None:
+        quads.indices(indices)
+    lv.display()
+    #lv.interactive()
+
+#1)
+V = lavavu.grid2d(dims=[3,3])
+print(V)
+print(V.shape)
+createGrid(V)
+
+#2)
+V = lavavu.grid3d(dims=[3,3])
+print(V)
+print(V.shape)
+
+createGrid(V)
+
+#3) Create some quads in a python list
+# - just provide vertices, no grid dimensions so will expect counter-clockwise quad vertex ordering
+def quad2(minv, maxv):
+    return [minv, [maxv[0], minv[1]], maxv, [minv[0], maxv[1]]]
+corners = []
+corners += quad2([0,0], [1,1])
+corners += quad2([1,0], [2,1])
+corners += quad2([0,1], [1,2])
+corners += quad2([1,1], [2,2])
+
+#4) Draw with vertices only
+createGrid(corners)
+
+#    0       1      2      3      4      5      6      7      8
+V = [[0,2], [1,2], [2,2], [0,1], [1,1], [2,1], [0,0], [1,0], [2,0]]
+I = [[3, 4, 1, 0], [4, 5, 2, 1], [6, 7, 4, 3], [7, 8, 5, 4]]
+
+#Use our own indices - colours will be interpolated
+createGrid(V, I)
+
+#5) With passed dims, auto index, grid layout expected:
+createGrid(V, None, dims=[3,3])
+
+#6) With calculated dims
+V = [[[0,2], [1,2], [2,2]], [[0,1], [1,1], [2,1]], [[0,0], [1,0], [2,0]]]
+createGrid(V)
+
+#Compare the output to expected results
+lv.testimages()
+
---
+++ planes/runtest.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+import lavavu
+lv = lavavu.Viewer(border=0, verbose=0, axis=True)
+
+x0 = 0
+y0 = 0
+z0 = 0
+x1 = 1
+y1 = 1
+z1 = 1
+
+#Canonical box: define the faces as if looking at them front on, starting from top left vertex
+#first dim changes first, then second dim, grid point order (row by row)
+#dims = [x,y] z fixed
+front_face = [[[x0, y1, z1], [x1, y1, z1]], [[x0, y0, z1], [x1, y0, z1]]] #Start from x0,y1
+back_face  = [[[x1, y1, z0], [x0, y1, z0]], [[x1, y0, z0], [x0, y0, z0]]] #Start from x1,y1 x order swapped from back view
+#dims = [x,z] y fixed
+top_face   = [[[x0, y1, z0], [x1, y1, z0]], [[x0, y1, z1], [x1, y1, z1]]] #Start from x0,z0
+bot_face   = [[[x0, y0, z1], [x1, y0, z1]], [[x0, y0, z0], [x1, y0, z0]]] #Start from x0,z1
+#dims = [z,y] x fixed
+left_face  = [[[x0, y1, z0], [x0, y1, z1]], [[x0, y0, z0], [x0, y0, z1]]] #Start from z0,y1
+right_face = [[[x1, y1, z1], [x1, y1, z0]], [[x1, y0, z1], [x1, y0, z0]]] #Start from z1,y1
+
+def box(g, **kwargs):
+    global lv
+    #lv.clear() #TODO: fix
+    lv = lavavu.Viewer(border=0, verbose=0, axis=True)
+
+    #Correct box
+    #Vertices interpreted as grid if dimensions provide (row by row), otherwise quads (counter clockwise travel per quad)
+    # dimensions can be provided with dims=[w,h] or by array shape
+    front   = lv.add("front", geometry=g, vertices=front_face, colour="blue", cullface=True, **kwargs)
+    back    = lv.add("back", geometry=g, vertices=back_face, colour="blue", cullface=True, **kwargs)
+    bottom  = lv.add("bottom", geometry=g, vertices=bot_face, colour="green", cullface=True, **kwargs)
+    top     = lv.add("top", geometry=g, vertices=top_face, colour="green", cullface=True, **kwargs)
+    left    = lv.add("left", geometry=g, vertices=left_face, colour="red", cullface=True, **kwargs)
+    right   = lv.add("right", geometry=g, vertices=right_face, colour="red", cullface=True, **kwargs)
+
+def test(out, exp):
+    global lv
+    #Enable transparent output
+    lv["pngalpha"] = True
+    lv.rotate('y', 45)
+    lv.rotate('x', 25)
+    lv.translation(0, 0.0, -2.25)
+    #lv.interactive()
+    lv.image(out, resolution=[300, 300])
+
+    if not lv.testimage("expected/" + exp, out):
+        raise RuntimeError("Image tests failed due to one or more image comparisons above tolerance level!")
+
+#Correct
+box("quads")
+test("quads.png", "box.png")
+
+#Inverse
+box("quads", flip=True)
+test("quads_r.png", "reverse.png")
+
+#Correct
+box("triangles", dims=[2,2])
+test("tris.png", "box.png")
+#Inverse
+box("triangles", flip=True, dims=[2,2])
+test("tris_r.png", "reverse.png")
+
+#Corner vertices of box, grid layout front then back
+verts = [[x0, y0, z1],
+         [x1, y0, z1],
+         [x0, y1, z1],
+         [x1, y1, z1],
+         [x0, y0, z0],
+         [x1, y0, z0],
+         [x0, y1, z0],
+         [x1, y1, z0]]
+
+#lv.clear() #TODO: fix
+lv = lavavu.Viewer(border=0, verbose=0, axis=True)
+
+#Reverse box that works in old and new (as correct box above)
+#Vertices interpreted as grid if dimensions provide (row by row), otherwise quads (counter clockwise travel per quad)
+# dimensions can be provided with dims=[w,h] or by array shape
+front   = lv.triangles(vertices=[[verts[0], verts[1]], [verts[2], verts[3]]], dims=[2,2], colour="blue", cullface=True)
+back    = lv.triangles(vertices=[[verts[5], verts[4]], [verts[7], verts[6]]], dims=[2,2], colour="blue", cullface=True)
+bottom  = lv.triangles(vertices=[[verts[0], verts[4]], [verts[1], verts[5]]], dims=[2,2], colour="green", cullface=True)
+top     = lv.triangles(vertices=[[verts[2], verts[3]], [verts[6], verts[7]]], dims=[2,2], colour="green", cullface=True)
+left    = lv.triangles(vertices=[[verts[4], verts[0]], [verts[6], verts[2]]], dims=[2,2], colour="red", cullface=True)
+right   = lv.triangles(vertices=[[verts[1], verts[5]], [verts[3], verts[7]]], dims=[2,2], colour="red", cullface=True)
+
+test("tris_r2.png", "reverse.png")
+
