f0b0dc18c1f23d85984905354a5fbfc504111a2e
Owen Kaluza <owen.kaluza@monash.edu>
2019-07-17 18:13:11 +1000

    Have to manually provide bounds for old database now

--- uw1-viewports/runtest.py
+++ uw1-viewports/runtest.py
@@ -7,9 +7,13 @@ import lavavu
 
 dbfile = "uw1-vp-gLucifer.gldb"
 
+#Bounds, no longer support loading from window table
+B0=[0., 0., 0.]
+B1=[1., 1., 0.]
+
 #Using image output scripted through python 
 #(NOTE: something weird happens if this runs last, so moved to first)
-lv = lavavu.Viewer(quality=1)
+lv = lavavu.Viewer(quality=1, min=B0, max=B1)
 lv.file(dbfile)
 
 lv.open()
@@ -21,12 +25,12 @@ for ts in range(300,501,100):
 lv.testimages()
 
 #Using automated image output
-lv = lavavu.Viewer(writeimage=True, timestep=[300, 500], database=dbfile, figure=-1, quality=1, port=0)
+lv = lavavu.Viewer(writeimage=True, timestep=[300, 500], database=dbfile, figure=-1, quality=1, port=0, min=B0, max=B1)
 
 lv.testimages()
 
 #Again using "images" command
-lv = lavavu.Viewer(database=dbfile, quality=1)
+lv = lavavu.Viewer(database=dbfile, quality=1, min=B0, max=B1)
 lv.timestep(300)
 lv.images(500)
 lv.testimages()
