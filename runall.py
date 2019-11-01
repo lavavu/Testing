370d984acfc466a4932fb4cb65812843018bbb3f
Owen Kaluza <owen.kaluza@monash.edu>
2019-11-01 14:47:44 +1100

    No doctests on python2
    If run the previous way, does not fail when the doctests fail so we need
    to check rather than rely on python2 check in lavavu.py __main__

--- runall.py
+++ runall.py
@@ -6,11 +6,13 @@ import subprocess
 path = os.getcwd()
 
 #Run the doctests
-print("Running doctests...")
-os.chdir(os.path.dirname(lavavu.__file__))
-#subprocess.check_call(["python", "lavavu.py"])
-subprocess.check_call(["python", "-m", "doctest", "-v", "lavavu.py"])
-os.chdir(path)
+if sys.version_info[0] < 3:
+    print("Python 2, Skip doctests")
+else:
+    print("Running doctests...")
+    os.chdir(os.path.dirname(lavavu.__file__))
+    subprocess.check_call(["python", "-m", "doctest", "-v", "lavavu.py"])
+    os.chdir(path)
 
 for d in sorted(os.listdir(path)):
     if not os.path.isdir(os.path.join(path,d)): continue
